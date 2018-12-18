aadffa7106bcf8dc30194ed5d4b001769d59cf24
Leigh Grainger <leighgrainger@gmail.com>
2018-12-17 19:39:55 -0700

    Updates

--- src/pages/Journal/Journal.js
+++ src/pages/Journal/Journal.js
@@ -7,61 +7,75 @@ import Hero from '../../components/Hero/Hero';
 import EntrySummary from '../../components/EntrySummary/EntrySummary';
 
 import './journal.css';
+import { renderStatic } from 'react-helmet';
 
 
-const Journal = ({ journalEntries }) => {
+class Journal extends React.Component {
 
-    
-    const [ url, entrySlug ] = window ? window.location.search.split( 'entry=' ) : [];
-    let journalEntry = null;
-
-    if ( Array.isArray( journalEntries ) && entrySlug ) {
-        const [ entry ] = journalEntries.filter( e => e.slug === entrySlug );
-        journalEntry = entry;
+    state = {
+        journalEntry: null
     }
 
-    return (
-        <div className="journal">
-            <Hero heading="Journal" />
+    componentDidMount() {
 
-            { journalEntry ?
-                <Section theme="white">
-                    <SectionPanel padded width="100%">
-                        <h2>{ journalEntry.title }</h2>
-                        <div dangerouslySetInnerHTML={{ __html: journalEntry.body }} />
-                    </SectionPanel>
-                </Section>
-            :
-                <Fragment>
-                    <Section theme="light-grain">
-                        <SectionPanel padded>
-                            <h2>Taking A Journey</h2>
-                            <p>
-                                We are on a journey to bring transformation and change to the people of 
-                                Haiti in the name of Jesus Christ. Here are our stories of the unfolding
-                                of that journey.
-                            </p>
-                        </SectionPanel>
-                    </Section>
+        const [ url, entrySlug ] = window.location.search.split( 'entry=' );
+
+        if ( Array.isArray( journalEntries ) && entrySlug ) {
+            const [ entry ] = journalEntries.filter( e => e.slug === entrySlug );
+            
+            this.setState({
+                journalEntry: entry
+            });
+        }
+    }
+
+    render() {
+
+        const { journalEntry } = this.state;
+
+        return (
+            <div className="journal">
+                <Hero heading="Journal" />
+
+                { journalEntry ?
                     <Section theme="white">
                         <SectionPanel padded width="100%">
-                            { Array.isArray( journalEntries ) && journalEntries.map(( entry, idx ) => (
-                                <EntrySummary
-                                    key={ `je-${ idx }` }
-                                    title={ entry.title }
-                                    author={ entry.author }
-                                    date={ entry.date }
-                                    thumbnail={ entry.thumbnail }
-                                    summary={ entry.summary }
-                                    articleLink={ `/journal?entry=${ entry.slug }` }
-                                />
-                            ))}
+                            <h2>{ journalEntry.title }</h2>
+                            <div dangerouslySetInnerHTML={{ __html: journalEntry.body }} />
                         </SectionPanel>
                     </Section>
-                </Fragment>
-             }
-        </div>
-    );
+                :
+                    <Fragment>
+                        <Section theme="light-grain">
+                            <SectionPanel padded>
+                                <h2>Taking A Journey</h2>
+                                <p>
+                                    We are on a journey to bring transformation and change to the people of 
+                                    Haiti in the name of Jesus Christ. Here are our stories of the unfolding
+                                    of that journey.
+                                </p>
+                            </SectionPanel>
+                        </Section>
+                        <Section theme="white">
+                            <SectionPanel padded width="100%">
+                                { Array.isArray( journalEntries ) && journalEntries.map(( entry, idx ) => (
+                                    <EntrySummary
+                                        key={ `je-${ idx }` }
+                                        title={ entry.title }
+                                        author={ entry.author }
+                                        date={ entry.date }
+                                        thumbnail={ entry.thumbnail }
+                                        summary={ entry.summary }
+                                        articleLink={ `/journal?entry=${ entry.slug }` }
+                                    />
+                                ))}
+                            </SectionPanel>
+                        </Section>
+                    </Fragment>
+                }
+            </div>
+        );
+    }
 };
 
 export default () => <RouteData component={ Journal } />;
